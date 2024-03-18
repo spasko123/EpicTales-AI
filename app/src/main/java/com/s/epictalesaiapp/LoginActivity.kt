27dedcb82eef79f9bb5ea2e105f43f3df5f1a336
spasko123 <spas86512@gmail.com>
2024-03-18 14:03:51 +0200

    first commit

---
+++ app/src/main/java/com/s/epictalesaiapp/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.s.epictalesaiapp
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            window.statusBarColor = android.graphics.Color.TRANSPARENT
+        }
+    }
+}
