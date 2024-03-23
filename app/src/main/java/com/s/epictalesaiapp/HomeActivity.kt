ddcdfaee33e7154e9949faccd2785fa97a920293
spasko123-Laptop <spas86512@gmail.com>
2024-03-23 14:32:44 +0200

    Spas Laptop

--- app/src/main/java/com/s/epictalesaiapp/HomeActivity.kt
+++ app/src/main/java/com/s/epictalesaiapp/HomeActivity.kt
@@ -1,11 +1,13 @@
 package com.s.epictalesaiapp
 
+import android.graphics.Color
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -14,16 +16,30 @@ class HomeActivity : AppCompatActivity() {
     lateinit var txtTest: TextView
     lateinit var auth: FirebaseAuth
     lateinit var db: FirebaseFirestore
+    lateinit var bottomNavMenu: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         txtTest = findViewById(R.id.txtTest)
+        bottomNavMenu = findViewById(R.id.bottomNavMenu)
+
+        bottomNavMenu.selectedItemId = R.id.home
 
         auth = FirebaseAuth.getInstance()
         db = FirebaseFirestore.getInstance()
         val currentUser = auth.currentUser
         val uid = currentUser?.uid
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // Set system UI flags to enable layout fullscreen and light status bar
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+
+            // Set status bar color to transparent
+            window.statusBarColor = Color.TRANSPARENT
+        }
+
 
 
 
